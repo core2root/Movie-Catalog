477674b40a1186f7d8a1845e0a4d7c7fd845470f
Maksim <maxnovikov51@gmail.com>
2020-01-14 19:54:56 +0200

    Basic dependency injection implementation

--- app/src/main/java/com/maksim/moviecatalog/app/MovieCatalogApp.kt
+++ app/src/main/java/com/maksim/moviecatalog/app/MovieCatalogApp.kt
@@ -2,22 +2,31 @@ package com.maksim.moviecatalog.app
 
 import android.app.Application
 import com.maksim.moviecatalog.app.di.AppContainer
+import com.maksim.moviecatalog.data.local.model.MyObjectBox
+import io.objectbox.BoxStore
 
 /**
  * Created by Maksim Novikov on 13-Jan-20.
  */
-class MovieCatalogApp: Application() {
+class MovieCatalogApp : Application() {
 
   private lateinit var appContainer: AppContainer
+  private var boxStore: BoxStore? = null
 
   override fun onCreate() {
     super.onCreate()
 
-    appContainer = AppContainer(applicationContext)
+    if (boxStore == null) {
+      boxStore = MyObjectBox.builder()
+        .androidContext(applicationContext)
+        .build()
+    }
+
+    appContainer = AppContainer(boxStore!!)
 
   }
 
-  fun getAppContainer(): AppContainer{
+  fun getAppContainer(): AppContainer {
     return appContainer
   }
 
--- app/src/main/java/com/maksim/moviecatalog/app/di/AppContainer.kt
+++ app/src/main/java/com/maksim/moviecatalog/app/di/AppContainer.kt
@@ -1,40 +1,73 @@
 package com.maksim.moviecatalog.app.di
 
-import android.content.Context
-import com.maksim.moviecatalog.app.ui.ViewModelFactory
+
+import androidx.lifecycle.ViewModelProviders
+import com.maksim.moviecatalog.app.base.BaseFragment
+import com.maksim.moviecatalog.app.ui.detail.MovieDetailViewModelFactory
+import com.maksim.moviecatalog.app.ui.detail.MovieDetailViewModel
+import com.maksim.moviecatalog.app.ui.movieList.MovieListType
+import com.maksim.moviecatalog.app.ui.movieList.MovieListViewModel
+import com.maksim.moviecatalog.app.ui.movieList.MovieListViewModelFactory
 import com.maksim.moviecatalog.data.MovieRepositoryImpl
+import com.maksim.moviecatalog.data.local.MovieDao
+import com.maksim.moviecatalog.data.local.MovieDaoImpl
+import com.maksim.moviecatalog.data.local.model.MovieEntity
+import com.maksim.moviecatalog.data.local.model.MovieEntityMapper
+import com.maksim.moviecatalog.data.local.model.MovieEntityMapperImpl
 import com.maksim.moviecatalog.data.remote.MovieService
 import com.maksim.moviecatalog.data.remote.MovieServiceImpl
 import com.maksim.moviecatalog.data.model.MovieMapper
 import com.maksim.moviecatalog.data.model.MovieMapperImpl
 import com.maksim.moviecatalog.domain.repository.MovieRepository
-import com.maksim.moviecatalog.domain.useCase.GetFavoriteMoviesUseCase
-import com.maksim.moviecatalog.domain.useCase.GetLatestMoviesUseCase
+import com.maksim.moviecatalog.domain.useCase.*
+import io.objectbox.BoxStore
 
 /**
  * Created by Maksim Novikov on 13-Jan-20.
  */
-class AppContainer(context: Context) {
+class AppContainer(boxStore: BoxStore) {
 
   private val movieRepository: MovieRepository
-  private val viewModelFactory: ViewModelFactory = ViewModelFactory(this)
 
   init {
     val movieMapper: MovieMapper = MovieMapperImpl()
-    val movieService: MovieService =
-      MovieServiceImpl()
-    movieRepository = MovieRepositoryImpl(movieService, movieMapper)
+    val movieService: MovieService = MovieServiceImpl()
+    val movieEntityMapper: MovieEntityMapper = MovieEntityMapperImpl()
+    val movieDao: MovieDao =
+      MovieDaoImpl(boxStore.boxFor(MovieEntity::class.java), movieEntityMapper)
+    movieRepository = MovieRepositoryImpl(movieService, movieDao, movieMapper)
   }
 
+  fun provideMovieDetailViewModel(fragment: BaseFragment, movieId: Int): MovieDetailViewModel {
+    val factory = MovieDetailViewModelFactory(this, movieId)
+    return ViewModelProviders.of(fragment, factory).get(MovieDetailViewModel::class.java)
+  }
+
+  fun provideMovieListViewModel(fragment: BaseFragment, type: MovieListType): MovieListViewModel {
+    val factory = MovieListViewModelFactory(this, type)
+    return ViewModelProviders.of(fragment, factory).get(MovieListViewModel::class.java)
+  }
 
   //Use case providers
 
-  fun provideGetLatestMoviesUseCase(): GetLatestMoviesUseCase {
-    return GetLatestMoviesUseCase(movieRepository)
+  fun provideUpdateMovieFavoriteStatusUseCase(): UpdateMovieFavoriteStatusUseCase {
+    return UpdateMovieFavoriteStatusUseCase(movieRepository)
+  }
+
+  fun provideGetMovieByIdUseCase(): GetMovieByIdUseCase {
+    return GetMovieByIdUseCase(movieRepository)
+  }
+
+  fun provideGetLatestMoviesUseCase(): GetTrendingMoviesUseCase {
+    return GetTrendingMoviesUseCase(movieRepository)
   }
 
   fun provideGetFavoriteMoviesUseCase(): GetFavoriteMoviesUseCase {
     return GetFavoriteMoviesUseCase(movieRepository)
+  }
+
+  fun provideGetAllMoviesUseCase(): GetAllMoviesLiveDataUseCase {
+    return GetAllMoviesLiveDataUseCase(movieRepository)
   }
 
 }
